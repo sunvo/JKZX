46bf7e19df281420978050e16cbe3d440d8d529b
日山河 <408977348@qq.com>
2015-12-18 16:28:17 +0800

    Initial Commit

---
+++ JKZX/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JKZX
+//
+//  Created by qianfeng on 15/12/18.
+//  Copyright © 2015年 张旭华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
